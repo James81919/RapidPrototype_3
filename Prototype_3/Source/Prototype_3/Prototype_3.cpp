bedca1c074855c0fce94646b35260e8a5490f37b
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2018-08-20 13:30:36 +0800

    Change to third person default

--- Prototype_3/Source/Prototype_3/Prototype_3.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Prototype_3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prototype_3, "Prototype_3" );
